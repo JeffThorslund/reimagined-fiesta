3915ec72bec13b0bd21e14a91e86f4e48f40bba6
Jeff Thorslund <54057907+JeffThorslund@users.noreply.github.com>
2023-07-17 16:17:16 +0000

    fix build

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { MantineProvider, Text } from '@mantine/core';
+import { MantineProvider } from '@mantine/core';
 
 function App() {
   return (
